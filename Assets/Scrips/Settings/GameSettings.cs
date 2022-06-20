5c64b193861e93326eff406248257babf16277fd
Unibo12 <uelefnuelns2@gmail.com>
2022-06-20 21:27:39 +0900

    ぷぅ...

--- Assets/Scrips/Settings/GameSettings.cs
+++ Assets/Scrips/Settings/GameSettings.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Settings/GameSettings", fileName = "GameSettings")]
 public sealed class GameSettings : ScriptableObject
 {
-    public bool isGamePadSetting; //ゲームパッドorキーボードの設定
+    public bool isGamePadSetting; // ゲームパッドorキーボードの設定
 
-    public float TopPlayerCameraX; //クロカンで先頭を走るキャラクタのカメラ位置調整
-    public float DeathTime; //失格時にキャラクターが消えるまでの時間
+    public float TopPlayerCameraX; // クロカンで先頭を走るキャラクタのカメラ位置調整
+    public float DeathTime; // 失格時にキャラクターが消えるまでの時間
 
     public int playerCount = 2; // プレイヤー数(CPUを除く)
 
